28c052551740d70c717d28f4a707c308073e2261
Julien Barnier <julien.barnier@cnrs.fr>
2024-04-05 18:20:25 +0200

    refactor: move DEFAULT_HEIGHT and DEFAULT_WIDTH to utils.py

--- src/pylifemap/utils.py
+++ src/pylifemap/utils.py
@@ -9,3 +9,6 @@ import polars as pl
 LMDATA_FILE = "data/lmdata.parquet"
 LMDATA_PATH = str(importlib.resources.files("pylifemap").joinpath(LMDATA_FILE))
 LMDATA = pl.read_parquet(LMDATA_PATH)
+
+DEFAULT_WIDTH = "100%"
+DEFAULT_HEIGHT = "600px"
