b7f02e47c83d873b465c1fc31a85ccfc29505509
Julien Barnier <julien.barnier@cnrs.fr>
2024-04-08 14:17:22 +0200

    feat: add is_notebook function

--- src/pylifemap/utils.py
+++ src/pylifemap/utils.py
@@ -10,5 +10,21 @@ LMDATA_FILE = "data/lmdata.parquet"
 LMDATA_PATH = str(importlib.resources.files("pylifemap").joinpath(LMDATA_FILE))
 LMDATA = pl.read_parquet(LMDATA_PATH)
 
-DEFAULT_WIDTH = "100%"
+DEFAULT_WIDTH = "800px"
 DEFAULT_HEIGHT = "600px"
+
+
+def is_notebook() -> bool:
+    """
+    Checks if we are currently in a notebook.
+
+    Returns
+    -------
+    bool
+        True if we are running in a notebook environment, False otherwise.
+    """
+    try:
+        test = get_ipython().__class__.__name__  # noqa: F841
+        return True
+    except NameError:
+        return False
