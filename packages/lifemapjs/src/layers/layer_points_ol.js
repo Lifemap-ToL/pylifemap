6b28f2904f7808a71c9bafb159e743b64e9cfe87
Julien Barnier <julien.barnier@cnrs.fr>
2025-10-15 17:10:08 +0200

    feat: add full features to layer_points_ol

--- packages/lifemapjs/src/layers/layer_points_ol.js
+++ packages/lifemapjs/src/layers/layer_points_ol.js
@@ -1,61 +1,203 @@
-import { guidGenerator } from "../utils";
+import { guidGenerator, DEFAULT_CAT_SCHEME, DEFAULT_NUM_SCHEME } from "../utils"
 
-import Feature from "ol/Feature.js";
-import Point from "ol/geom/Point.js";
-import Vector from "ol/source/Vector.js";
-import WebGLPointsLayer from "ol/layer/WebGLPoints.js";
-import { fromLonLat } from "ol/proj.js";
+import Feature from "ol/Feature.js"
+import Point from "ol/geom/Point.js"
+import WebGLVectorLayer from "ol/layer/WebGLVector.js"
+import { fromLonLat } from "ol/proj.js"
 
-import * as d3 from "d3";
-import * as Plot from "@observablehq/plot";
+import * as d3 from "d3"
+import * as Plot from "@observablehq/plot"
+import VectorSource from "ol/source/Vector.js"
 
 export function layer_points_ol(map, data, options = {}) {
     let {
         id = null,
         x_col = "pylifemap_x",
         y_col = "pylifemap_y",
-        radius = null,
+        radius = 5,
         radius_col = null,
         fill_col = null,
         fill_col_cat = null,
+        label = null,
         scheme = null,
-        opacity = 0.1,
+        opacity = 0.8,
         popup = false,
-    } = options;
+        hover = false,
+        radius_range = [1, 20],
+    } = options
 
-    let scales = [];
-    let popup_obj = map.popup;
-    id = `lifemap-ol-${id ?? guidGenerator()}`;
+    let scales = []
+    let popup_obj = map.popup
+    id = `lifemap-ol-${id ?? guidGenerator()}`
 
-    const n_features = data.length;
-    const features = new Array(n_features);
+    console.log(radius_range)
+    // Radius function
+    let get_radius_col_fn = function (data, radius_col) {
+        if (radius_col == null) {
+            return null
+        }
+        const min_domain = d3.min(data, (d) => Number(d[radius_col]))
+        const max_domain = d3.max(data, (d) => Number(d[radius_col]))
+        const [min_range, max_range] = radius_range
 
+        const fn = (d) => {
+            return (
+                min_range +
+                ((Number(d) - min_domain) / (max_domain - min_domain)) *
+                    (max_range - min_range)
+            )
+        }
+        return fn
+    }
+
+    // Fill color function
+    let get_fill_col_fn = function (data, fill_col, fill_col_cat) {
+        if (fill_col == null) {
+            return null
+        }
+        // Determine if scale is categorical or linear
+        if (fill_col_cat === null) {
+            fill_col_cat = !(
+                ["number", "bigint"].includes(typeof data[0][fill_col]) &
+                ([...new Set(data.map((d) => d[fill_col]))].length > 10)
+            )
+        }
+        let fn
+        // Linear color scale
+        if (!fill_col_cat) {
+            const max_value = d3.max(data, (d) => Number(d[fill_col]))
+            const min_value = d3.min(data, (d) => Number(d[fill_col]))
+            scheme = scheme ?? DEFAULT_NUM_SCHEME
+            const scale = {
+                color: {
+                    type: "linear",
+                    scheme: scheme,
+                    domain: [min_value, max_value],
+                },
+                className: "lifemap-ol-lin-legend",
+                label: label ?? fill_col,
+            }
+            scales.push(scale)
+            fn = (d) => Plot.scale(scale).apply(Number(d))
+        }
+        // Categorical color scale
+        else {
+            scheme = scheme ?? DEFAULT_CAT_SCHEME
+            const domain = [...new Set(data.map((d) => d[fill_col]))].sort()
+            const scale = {
+                color: { type: "categorical", scheme: scheme, domain: domain },
+                columns: 1,
+                className: "lifemap-ol-cat-legend",
+                label: label ?? fill_col,
+            }
+            scales.push(scale)
+            fn = (d) => Plot.scale(scale).apply(d)
+        }
+        return fn
+    }
+
+    // Create features
+    const n_features = data.length
+    const features = new Array(n_features)
+    const radius_col_fn = get_radius_col_fn(data, radius_col)
+    const fill_col_fn = get_fill_col_fn(data, fill_col, fill_col_cat)
     for (let i = 0; i < n_features; i++) {
-        let line = data[i];
-        const coordinates = fromLonLat([line[x_col], line[y_col]]);
+        let line = data[i]
+        const coordinates = fromLonLat([line[x_col], line[y_col]])
         features[i] = new Feature({
             geometry: new Point(coordinates),
-        });
+            data: line,
+        })
+        if (radius_col_fn != null) {
+            features[i].set("radius_col", radius_col_fn(line[radius_col]))
+        }
+        if (fill_col != null) {
+            features[i].set("fill_col", fill_col_fn(line[fill_col]))
+        }
     }
-    const source = new Vector({
+    const source = new VectorSource({
         features: features,
-    });
+    })
+
+    // Radius style
+    let circle_radius
+    if (radius_col !== null) {
+        circle_radius = ["get", "radius_col"]
+    } else {
+        circle_radius = radius
+    }
+    // Fill style
+    let circle_fill_color
+    if (fill_col !== null) {
+        circle_fill_color = ["get", "fill_col"]
+    } else {
+        circle_fill_color = "#DD0000"
+    }
+    if (hover) {
+        circle_fill_color = ["match", ["get", "hover"], 1, "#ff0000", circle_fill_color]
+    }
 
     const style = {
-        "circle-radius": radius,
-        "circle-fill-color": "#FF0000",
+        "circle-radius": circle_radius,
+        "circle-fill-color": circle_fill_color,
         "circle-opacity": opacity,
-    };
+    }
 
     // Layer definition
-    const layer = new WebGLPointsLayer({
+    const layer = new WebGLVectorLayer({
         source: source,
         style: style,
-        disableHitDetection: true,
-    });
+        disableHitDetection: false,
+        declutter: true,
+    })
 
-    layer.lifemap_ol_id = id;
-    layer.lifemap_ol_layer = true;
-    layer.lifemap_ol_scales = scales;
-    return layer;
+    // Hover
+    let selected_feature = null
+    if (hover) {
+        map.on("pointermove", function (ev) {
+            if (selected_feature !== null) {
+                selected_feature.set("hover", 0)
+                selected_feature = null
+            }
+
+            map.forEachFeatureAtPixel(ev.pixel, function (feature) {
+                feature.set("hover", 1)
+                selected_feature = feature
+                return true
+            })
+        })
+    }
+
+    // Popup
+    if (popup) {
+        map.on("click", function (evt) {
+            const feature = map.forEachFeatureAtPixel(evt.pixel, (feature) => feature)
+            if (!feature) {
+                return
+            }
+            map.dispose_popup()
+            let content = `<table><tbody><tr><td class='right'><strong>TaxId:</td><td>${feature.get("data").taxid}</td></tr>`
+            content +=
+                radius_col !== null && radius_col != fill_col
+                    ? `<tr><td class='right'><strong>${radius_col}:</strong></td><td>${feature.get("data")[radius_col]}</td></tr>`
+                    : ""
+            content += fill_col
+                ? `<tr><td class='right'><strong>${fill_col}:</strong></td><td>${feature.get("data")[fill_col]}</td></tr>`
+                : ""
+            content += "</tbody></table>"
+
+            const coordinates = [
+                feature.get("data").pylifemap_x,
+                feature.get("data").pylifemap_y,
+            ]
+            const offset = [0, -5]
+            map.show_popup(coordinates, content, offset)
+        })
+    }
+
+    layer.lifemap_ol_id = id
+    layer.lifemap_ol_layer = true
+    layer.lifemap_ol_scales = scales
+    layer.is_webgl = true
+    return layer
 }
