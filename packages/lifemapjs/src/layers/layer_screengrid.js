ae7e35c1c7e6d02f134e405611d96a03babbaac2
Julien Barnier <julien.barnier@cnrs.fr>
2025-10-28 16:16:35 +0100

    cleanup: rename layer_screen_grid to layer_screengrid and remove unused layer_grid

--- packages/lifemapjs/src/layers/layer_screengrid.js
+++ packages/lifemapjs/src/layers/layer_screengrid.js
@@ -1,7 +1,7 @@
-import { ScreenGridLayer } from "@deck.gl/aggregation-layers";
-import { guidGenerator } from "../utils";
+import { ScreenGridLayer } from "@deck.gl/aggregation-layers"
+import { guidGenerator } from "../utils"
 
-export function layer_screengrid(map, data, options = {}) {
+export function layer_screengrid(data, options = {}) {
     let {
         id = undefined,
         x_col = "pylifemap_x",
@@ -9,9 +9,9 @@ export function layer_screengrid(map, data, options = {}) {
         cell_size = 30,
         opacity = 0.5,
         extruded = false,
-    } = options;
+    } = options
 
-    id = `lifemap-ol-${id ?? guidGenerator()}`;
+    id = `lifemap-ol-${id ?? guidGenerator()}`
 
     const layer = new ScreenGridLayer({
         data: data,
@@ -22,8 +22,8 @@ export function layer_screengrid(map, data, options = {}) {
         cellSizePixels: cell_size,
         extruded: extruded,
         opacity: opacity,
-    });
+    })
 
-    layer.lifemap_ol_id = id;
-    return layer;
+    layer.lifemap_ol_id = id
+    return layer
 }
