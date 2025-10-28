0229f206da0f5a327cadaeba2a6531bb1387adf4
Julien Barnier <julien.barnier@cnrs.fr>
2025-10-28 10:48:21 +0100

    impr: reset zoom control styling tweaks

--- packages/lifemapjs/src/controls.js
+++ packages/lifemapjs/src/controls.js
@@ -3,17 +3,17 @@ import { DEFAULT_LAT, DEFAULT_LON } from "./utils"
 import { fromLonLat } from "ol/proj"
 
 export class ResetZoomControl extends Control {
-    /**
-     * @param {Object} [opt_options] Control options.
-     */
     constructor(opt_options) {
         const options = opt_options || {}
 
         const button = document.createElement("button")
-        button.innerHTML = "⌂"
+        button.setAttribute("title", "Reset zoom")
+        button.innerHTML =
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" /></svg>'
 
         const element = document.createElement("div")
         element.className = "reset-zoom pylifemap-control ol-unselectable ol-control"
+        element.style.top = "65px"
         element.appendChild(button)
 
         super({
