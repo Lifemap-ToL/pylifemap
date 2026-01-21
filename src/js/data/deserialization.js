83ee4eaef8e9f4edc6c4ab1cea46ae1f3ca36495
Julien Barnier <julien.barnier@cnrs.fr>
2026-01-21 13:22:09 +0100

    cleanup: deserialization.js code

--- src/js/data/deserialization.js
+++ src/js/data/deserialization.js
@@ -19,22 +19,7 @@ compressionRegistry.set(CompressionType.LZ4_FRAME, lz4Codec)
 // Unserialize data from Arrow IPC to JS Array
 export function deserialize_data(data) {
     if (data["serialized"]) {
-        let value = data["value"]
-        let table = tableFromIPC(value)
-        table = table.toArray()
-        // Find timestamp column names
-        //const date_columns = table.schema.fields
-        //    .filter((d) => d.type.toString().startsWith("Timestamp"))
-        //    .map((d) => d.name);
-        // Convert to JS array (it is done by Plot afterward anyway)
-        // Convert timestamp columns to Date
-        //table = table.map((d) => {
-        //    for (let col of date_columns) {
-        //        d[col] = new Date(d[col]);
-        //    }
-        //   return d;
-        //});
-        return table
+        return tableFromIPC(data["value"]).toArray()
     } else {
         return data["value"]
     }
