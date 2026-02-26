80178f44817f49bf9fa4768490580ad5c75e4753
Julien Barnier <julien.barnier@cnrs.fr>
2026-02-26 14:37:12 +0100

    maint: add watch mode to js build script

--- packages/lifemap-js/esbuild.config.js
+++ packages/lifemap-js/esbuild.config.js
@@ -1,14 +1,21 @@
 import esbuild from "esbuild"
 
-esbuild
-    .build({
-        entryPoints: ["index.js"], // Your entry point
-        bundle: true,
-        outfile: "dist/lifemap.js",
-        format: "iife",
-        globalName: "Lifemap",
-        minify: true,
-        platform: "browser",
-        sourcemap: true,
-    })
-    .catch(() => process.exit(1))
+let ctx = await esbuild.context({
+    entryPoints: ["index.js"], // Your entry point
+    bundle: true,
+    outfile: "dist/lifemap.js",
+    format: "iife",
+    globalName: "Lifemap",
+    minify: true,
+    platform: "browser",
+    sourcemap: true,
+})
+
+await ctx.rebuild().catch(() => process.exit(1))
+
+if (process.env.WATCH === "true") {
+    await ctx.watch()
+    console.log("Watching...")
+} else {
+    ctx.dispose()
+}
