5afdf4e5ffe93b40495ab0859f05f26d1e1159bc
Julien Barnier <julien.barnier@cnrs.fr>
2026-08-19 14:12:15 +0200

    fix: undefined and null comparisons in update_coordinates

--- packages/lifemap-js/src/data/update_coordinates.js
+++ packages/lifemap-js/src/data/update_coordinates.js
@@ -10,10 +10,10 @@ export async function update_coordinates(data) {
                 data[k]
                     .map((d) =>
                         // If lines data, add parent
-                        d.pylifemap_parent !== undefined
+                        d.pylifemap_parent != null
                             ? [d.pylifemap_taxid, d.pylifemap_parent]
                             : // If arcs data, add dest
-                              d.pylifemap_dest_taxid !== undefined
+                              d.pylifemap_dest_taxid != null
                               ? [d.pylifemap_taxid, d.pylifemap_dest_taxid]
                               : d.pylifemap_taxid
                     )
@@ -33,25 +33,25 @@ export async function update_coordinates(data) {
                 // Filter out data points whose taxid is not in the lifemap database anymore
                 data[k] = data[k].filter((d) => {
                     const taxid_coords = coords[d.pylifemap_taxid]
-                    if (taxid_coords === undefined) {
+                    if (taxid_coords == null) {
                         console.warn(
                             `${d.pylifemap_taxid} not found in updated coords - removed`
                         )
                         return false
                     }
                     // Lines data
-                    if (d.pylifemap_parent_taxid !== undefined) {
+                    if (d.pylifemap_parent_taxid != null) {
                         const taxid_parent_coords = coords[d.pylifemap_parent_taxid]
-                        if (taxid_parent_coords === undefined) {
+                        if (taxid_parent_coords == null) {
                             console.warn(
                                 `${d.pylifemap_parent_taxid} not found in updated parent coords - removed`
                             )
                             return false
                         }
                     }
-                    if (d.pylifemap_dest_taxid !== undefined) {
+                    if (d.pylifemap_dest_taxid != null) {
                         const taxid_dest_coords = coords[d.pylifemap_dest_taxid]
-                        if (taxid_dest_coords === undefined) {
+                        if (taxid_dest_coords == null) {
                             console.warn(
                                 `${d.pylifemap_dest_taxid} not found in updated dest coords - removed`
                             )
@@ -64,23 +64,23 @@ export async function update_coordinates(data) {
                 // Update coordinates for still existing taxids
                 data[k].forEach((d) => {
                     const taxid_coords = coords[d.pylifemap_taxid]
-                    if (d.pylifemap_zoom !== undefined) {
+                    if (d.pylifemap_zoom != null) {
                         d.pylifemap_zoom = taxid_coords.zoom
                     }
-                    if (d.pylifemap_x !== undefined) {
+                    if (d.pylifemap_x != null) {
                         d.pylifemap_x = taxid_coords.x
                         d.pylifemap_y = taxid_coords.y
                     }
 
                     // Lines data
-                    if (d.pylifemap_parent_taxid !== undefined) {
+                    if (d.pylifemap_parent_taxid != null) {
                         const taxid_parent_coords = coords[d.pylifemap_parent_taxid]
                         d.pylifemap_parent_x = taxid_parent_coords.x
                         d.pylifemap_parent_y = taxid_parent_coords.y
                     }
 
                     // Arcs data
-                    if (d.pylifemap_dest_taxid !== undefined) {
+                    if (d.pylifemap_dest_taxid != null) {
                         const taxid_dest_coords = coords[d.pylifemap_dest_taxid]
                         d.pylifemap_dest_x = taxid_dest_coords.x
                         d.pylifemap_dest_y = taxid_dest_coords.y
