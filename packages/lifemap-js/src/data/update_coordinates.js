c8ca8c3b8e485f06a39ddf18a38a931307ac8b98
Julien Barnier <julien.barnier@cnrs.fr>
2026-08-19 13:38:11 +0200

    fix: filter out unknown data points when updating coordinates

--- packages/lifemap-js/src/data/update_coordinates.js
+++ packages/lifemap-js/src/data/update_coordinates.js
@@ -5,10 +5,9 @@ import { get_data_coords } from "./api"
 export async function update_coordinates(data) {
     let taxids = new Set()
     for (let k in data) {
-        let current_data = data[k]
         taxids = taxids.union(
             new Set(
-                current_data
+                data[k]
                     .map((d) =>
                         // If lines data, add parent
                         d.pylifemap_parent !== undefined
@@ -31,12 +30,40 @@ export async function update_coordinates(data) {
         // If query succeeded, update coordinates with new values
         if (coords !== null) {
             for (let k in data) {
-                data[k].forEach((d) => {
+                // Filter out data points whose taxid is not in the lifemap database anymore
+                data[k] = data[k].filter((d) => {
                     const taxid_coords = coords[d.pylifemap_taxid]
                     if (taxid_coords === undefined) {
-                        console.warn(`${d.pylifemap_taxid} not found in updated coords`)
-                        return
+                        console.warn(
+                            `${d.pylifemap_taxid} not found in updated coords - removed`
+                        )
+                        return false
                     }
+                    // Lines data
+                    if (d.pylifemap_parent_taxid !== undefined) {
+                        const taxid_parent_coords = coords[d.pylifemap_parent_taxid]
+                        if (taxid_parent_coords === undefined) {
+                            console.warn(
+                                `${d.pylifemap_parent_taxid} not found in updated parent coords - removed`
+                            )
+                            return false
+                        }
+                    }
+                    if (d.pylifemap_dest_taxid !== undefined) {
+                        const taxid_dest_coords = coords[d.pylifemap_dest_taxid]
+                        if (taxid_dest_coords === undefined) {
+                            console.warn(
+                                `${d.pylifemap_dest_taxid} not found in updated dest coords - removed`
+                            )
+                            // Filter out data point
+                            return false
+                        }
+                    }
+                    return true
+                })
+                // Update coordinates for still existing taxids
+                data[k].forEach((d) => {
+                    const taxid_coords = coords[d.pylifemap_taxid]
                     if (d.pylifemap_zoom !== undefined) {
                         d.pylifemap_zoom = taxid_coords.zoom
                     }
@@ -48,31 +75,15 @@ export async function update_coordinates(data) {
                     // Lines data
                     if (d.pylifemap_parent_taxid !== undefined) {
                         const taxid_parent_coords = coords[d.pylifemap_parent_taxid]
-                        if (taxid_parent_coords === undefined) {
-                            console.warn(
-                                `${d.pylifemap_parent_taxid} not found in updated parent coords`
-                            )
-                            return
-                        }
-                        if (taxid_parent_coords !== undefined) {
-                            d.pylifemap_parent_x = taxid_parent_coords.x
-                            d.pylifemap_parent_y = taxid_parent_coords.y
-                        }
+                        d.pylifemap_parent_x = taxid_parent_coords.x
+                        d.pylifemap_parent_y = taxid_parent_coords.y
                     }
 
                     // Arcs data
                     if (d.pylifemap_dest_taxid !== undefined) {
                         const taxid_dest_coords = coords[d.pylifemap_dest_taxid]
-                        if (taxid_dest_coords === undefined) {
-                            console.warn(
-                                `${d.pylifemap_dest_taxid} not found in updated dest coords`
-                            )
-                            return
-                        }
-                        if (taxid_dest_coords !== undefined) {
-                            d.pylifemap_dest_x = taxid_dest_coords.x
-                            d.pylifemap_dest_y = taxid_dest_coords.y
-                        }
+                        d.pylifemap_dest_x = taxid_dest_coords.x
+                        d.pylifemap_dest_y = taxid_dest_coords.y
                     }
                 })
             }
